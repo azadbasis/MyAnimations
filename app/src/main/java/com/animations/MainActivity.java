6b5fdaf196500d1eb4e6c4ed95aa513099ec7d3d
Azhar <azadbasis@gmail.com>
2019-03-08 01:34:39 +0600

    Create An Animator Resource

--- app/src/main/java/com/animations/MainActivity.java
+++ app/src/main/java/com/animations/MainActivity.java
@@ -1,5 +1,6 @@
 package com.animations;
 
+import android.animation.AnimatorInflater;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private View container;
     private View welcome;
     private View profilePic;
+    private View signIn;
     private boolean playAnimations = true;
 
     @Override
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         container = findViewById(R.id.container);
         welcome = findViewById(R.id.welcome);
         profilePic = findViewById(R.id.profile_pic);
+        signIn = findViewById(R.id.sign_in);
     }
 
 
@@ -39,24 +42,28 @@ public class MainActivity extends AppCompatActivity {
         container.animate().alpha(1f).setDuration(1000);
     }
 
-    private void showOtherItem(){
+    private void showOtherItem() {
 
-        float startXWelcome = 0-welcome.getWidth();
+        float startXWelcome = 0 - welcome.getWidth();
         float endXWelcome = welcome.getX();
 
-        ObjectAnimator animWelcome=ObjectAnimator.ofFloat(welcome,View.X,startXWelcome,endXWelcome);
+        ObjectAnimator animWelcome = ObjectAnimator.ofFloat(welcome, View.X, startXWelcome, endXWelcome);
         animWelcome.setDuration(1500);
         welcome.setVisibility(View.VISIBLE);
         animWelcome.start();
 
         PropertyValuesHolder scaleXHolder =
-                        PropertyValuesHolder.ofFloat(View.SCALE_X,1f);
-        PropertyValuesHolder scaleYHolder = PropertyValuesHolder.ofFloat(View.SCALE_Y,1f);
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 1f);
+        PropertyValuesHolder scaleYHolder = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f);
 
-        ObjectAnimator animProfile=
-                     ObjectAnimator.ofPropertyValuesHolder(profilePic,scaleXHolder,scaleYHolder);
+        ObjectAnimator animProfile =
+                ObjectAnimator.ofPropertyValuesHolder(profilePic, scaleXHolder, scaleYHolder);
         animProfile.setDuration(1500);
         animProfile.start();
 
+        ObjectAnimator animSignIn =
+                (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.sign_in_animator);
+        animSignIn.setTarget(signIn);
+        animSignIn.start();
     }
 }
