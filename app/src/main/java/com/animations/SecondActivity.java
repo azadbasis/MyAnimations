7c79fa21c3c4f8494b1e910cee66583d481d7a0e
Azhar <azadbasis@gmail.com>
2019-03-08 04:23:22 +0600

    CreateSceneTransition(AndroidTransitionFramework).

--- app/src/main/java/com/animations/SecondActivity.java
+++ app/src/main/java/com/animations/SecondActivity.java
@@ -2,12 +2,42 @@ package com.animations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Scene;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SecondActivity extends AppCompatActivity {
+
+
+    private LinearLayout sceneHolder;
+    private Scene all;
+    private Scene office;
+    private Scene gyme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        sceneHolder = (LinearLayout) findViewById(R.id.scenes_holder);
+        all = Scene.getSceneForLayout(sceneHolder, R.layout.all_scene, this);
+        office = Scene.getSceneForLayout(sceneHolder, R.layout.office_scene, this);
+        gyme = Scene.getSceneForLayout(sceneHolder, R.layout.gyme_scene, this);
+    }
+
+
+    public void showAllScene(View view) {
+        TransitionManager.go(all, new ChangeBounds());
+    }
+
+    public void shoOfficeScene(View view) {
+        TransitionManager.go(office, new ChangeBounds());
+
+    }
+
+    public void showGymeScene(View view) {
+        TransitionManager.go(gyme, new ChangeBounds());
+
     }
 }
